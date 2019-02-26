d6868de25688b76d20009bdbd08ef73aab28af70
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2019-02-26 04:11:43 +0300

    parallelized evo fns

--- GRU_neuroevo.jl
+++ GRU_neuroevo.jl
@@ -1,4 +1,5 @@
 include("GRU_api.jl")
+# TODO : use pmap
 
 
 
@@ -13,12 +14,11 @@ begin
     hm_classes = length(noises)
     scores = [0.0 for _ in 1:hm_classes]
     label = [i == wrt ? 0.0 : 1.0 for i in 1:hm_classes]
-    for (i,noise) in enumerate(noises)
+    @distributed (vcat) for noise in noises
         out = prop(model, noise)
-        # scores[i] = argmax(reshape(result, hm_classes))
-        scores[i] = - sum(label .* log.(out))
+    [cross_entropy(out, label)]
     end
-scores
+# reshape(scores, length())
 end
 
 
@@ -29,28 +29,31 @@ begin
         @diff begin
             out = prop(model, sequence) # label = argmax(reshape(out, length(out)))
             label = [i == class ? 1.0 : 0.0 for i in 1:length(out)]
-            loss  = cross_entropy(out, label)
+            cross_entropy(out, label)
     end
     sequence = [value(t - grad(result,t)*lr) for t in sequence]
 sequence
 end
 
 
-mutate(noise; prob=.1) =
+mutate(noises, rate, prob) =
 begin
-    new_noise = []
-    for (it,t) in enumerate(noise)
-        timestep = []
-        for (iv,v) in enumerate(t)
-            if rand() <= prob
-                v += randn()
+    new_noises = @distributed (vcat) for noise in noises
+        new_noise = []
+        for (it,t) in enumerate(noise)
+            timestep = []
+            for (iv,v) in enumerate(t)
+                if rand() <= prob
+                    v += randn() * rate
+                end
+                push!(timestep, v)
             end
-            push!(timestep, v)
+            timestep = reshape(timestep, 1, length(timestep))
+            push!(new_noise, timestep)
         end
-        timestep = reshape(timestep, 1, length(timestep))
-        push!(new_noise, timestep)
+        [new_noise]
     end
-new_noise
+new_noises
 end
 
 
@@ -69,23 +72,29 @@ fits
 end
 
 
-crossover(fits, rate) =
+crossover(fits, prob) =
 begin
     offsprings = []
     for fit1 in fits
-        for fit2 in fits
+        @distributed (vcat) for fit2 in fits
             if fit1 != fit2
                 offspring = []
-                for (t1,t2) in zip(fit1, fit2) # TODO : improve.
-                    if rand() <= rate
-                        push!(offspring, t1)
-                    else
-                        push!(offspring, t2)
+                for (t1,t2) in zip(fit1, fit2)
+                    timestep = []
+                    for (e1,e2) in zip(t1,t2)
+                        if rand() <= prob
+                            push!(timestep, e2)
+                        else
+                            push!(timestep, e1)
+                        end
+                    push!(offspring, timestep)
+                    # [timestep]
                     end
                 end
-                push!(offsprings, offspring)
+                # push!(offsprings, offspring)
+            [offspring]
             end
         end
     end
-vcat(fits, offsprings)
+offsprings
 end
--- G_run.jl
+++ G_run.jl
@@ -20,8 +20,9 @@ const data = [[[randn(1, in_size) for i in 1:rand(1:16)], softmax(randn(1, out_s
 
 
 
-for _ in 1:hm_epochs
+for i in 1:hm_epochs
 
+    print("epoch: $i ")
     train!(model, data, learning_rate)
 
 end
@@ -32,19 +33,22 @@ end
 
 
 
-const hm_population  = 50
-const hm_mostfit     = 10
+const class = 4
 
-const track_length   = 16
-const size_per_time  = 42
+const hm_population  = 50
+const hm_mostfit     = 5
+
+const track_length   = 8
+const size_per_time  = in_size
+const hm_classes     = out_size
 
 const hm_generations = 10
-const hm_total_loop  = 20
+const hm_total_loop  = 100
 
-const crossover_rate = 0.2
-const update_rate    = 0.1
-
-const class = 4
+const crossover_prob = .2
+const mutate_prob    = .5
+const mutate_rate    = .2
+const update_rate    = .1
 
 
 
@@ -54,34 +58,41 @@ noises = [noise(track_length, size_per_time) for _ in 1:hm_population]
 
 evolve(noises, iterations) =
 begin
-    # loss_init = sum(scores(model, noises, class))
-
     for i in 1:iterations
-        println("Iterating: ", i)
 
-        noises = mostfit(noises, hm_mostfit, model, class)
-        noises = crossover(noises, crossover_rate)
-        noises = [update(noise, model, update_rate, class) for noise in noises]
-        noises = [mutate(noise) for noise in noises]
+        fits = mostfit(noises, hm_mostfit, model, class)
+        # trained_fits = [update(noise, model, update_rate, class) for noise in fits]
+        produced = crossover(fits, crossover_prob)
+        noises = vcat(noises, produced)
+        noises = mutate(noises, mutate_rate, mutate_prob)
+        noises = vcat(noises, fits)
+        # noises = vcat(noises, other_noises) # for noise in other_noises push!(noises, noise) end
         noises = mostfit(noises, hm_population, model, class)
 
+        print("/")
     end
+    print("\n")
 
-    loss = scores(model, noises, class)
-    # println("Progress: ", 1-sum(loss)/loss_init)
-
-    @show sum(loss)
-noises
+    loss = sum(scores(model, mostfit(noises, hm_mostfit, model, class), class)/hm_mostfit)
+    # loss = scores(model, mostfit(noises, 1, model, class), class)
+    @show loss
+[noises, loss]
 end
 
 
 loop(noises, hm_loop, hm_generations) =
+begin
+    loss_init = sum(scores(model, mostfit(noises, hm_mostfit, model, class), class))
 
     for i in 1:hm_loop
-        println("Looping: ", i)
+        print("loop: ",i," ")
 
-        noises = evolve(noises, hm_generations)
+        noises, loss = evolve(noises, hm_generations)
+
+        println("Progress: ", (1-loss/loss_init)*100)
     end
+noises
+end
 
 
 
